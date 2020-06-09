317c4f5bafcda3f794f32babf06db1de103e4195
forfed <maxime.fedou@gmail.com>
2020-06-09 21:57:52 +0200

    rajout de la barre deroulante

---
+++ dist/app.js
@@ -0,0 +1,76 @@
+function goto(ligne) {
+    document.querySelector(ligne).classList.toggle("hidden");
+}
+var hmenu = document.getElementById('h-menu')
+var listmenu = document.getElementById('menulist')
+listmenu.style.display = 'none'
+hmenu.addEventListener('click', () => {
+    var listmenu = document.getElementById('menulist')
+    if (listmenu.style.display === "" || listmenu.style.display === "none") {
+
+        listmenu.style.display = "block"
+
+    } else {
+
+
+        listmenu.style.display = "none"
+
+    }
+
+
+
+
+
+
+
+
+})
+
+
+
+window.addEventListener('scroll', function(e) {
+    window.requestAnimationFrame(function() {
+        if (window.scrollY > 400) {
+            var logo = document.getElementsByClassName('Navbar__logo')[0]
+            var navbar = document.getElementsByTagName('nav')[0]
+            navbar.style.position = 'fixed'
+            navbar.style.zIndex = "100"
+            navbar.style.backgroundColor = "#003049ff"
+            logo.style.display = "none"
+
+        } else {
+            var logo = document.getElementsByClassName('Navbar__logo')[0]
+            logo.style.display = "block"
+
+            var navbar = document.getElementsByTagName('nav')[0]
+            navbar.style.position = 'static'
+            navbar.style.zIndex = ""
+            navbar.style.opacity = ""
+            navbar.style.backgroundColor = "#003049"
+
+
+        }
+
+
+
+
+
+    })
+})
+
+
+
+var a = document.getElementsByTagName("a")
+for (var item of a) {
+
+    item.addEventListener('click', () => {
+        var menu = document.getElementById('menulist')
+
+        menu.style.display = "none"
+
+
+
+
+    })
+
+}
